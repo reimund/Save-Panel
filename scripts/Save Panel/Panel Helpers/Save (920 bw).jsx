be4e4355bf6c6fce9ce21206e7b4ae3c847b434c
Reimund Trost <reimund@code7.se>
2013-07-17 08:14:16 +0200

    Removed obsolete files.

--- scripts/Save Panel/Panel Helpers/Save (920 bw).jsx
+++
@@ -1,6 +0,0 @@
-$.evalFile(app.path + '/Presets/Scripts/Save%20Panel/Save%20Panel%20Only/Saver.jsx');
-s.save({
-	format: 'webjpg',
-	size: '920',
-	suffix: '_bw'
-});
--- scripts/Save Panel/Panel Helpers/Save (full bw).jsx
+++
@@ -1,6 +0,0 @@
-$.evalFile(app.path + '/Presets/Scripts/Save%20Panel/Save%20Panel%20Only/Saver.jsx');
-s.save({
-	format: 'jpg',
-	size: 'full',
-	suffix: '_bw'
-});
